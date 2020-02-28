a8c87afe5f95a0db3b666f9bb6c40246aa0704cf
yks095 <rltjr219@naver.com>
2020-02-28 19:06:16 +0900

    First commit for Querydsl

---
+++ src/test/java/com/kiseok/studyquerydsl/StudyQuerydslApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kiseok.studyquerydsl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyQuerydslApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
